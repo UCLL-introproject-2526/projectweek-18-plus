4c599f6444d2bda0fddbdf4f3198e1ef93e76577
Yara Claesen <yara.claesen@student.ucll.be>
2025-12-15 15:31:21 +0100

    update app.py

--- app.py
+++ app.py
@@ -1,20 +1,28 @@
 import pygame
-def main():
+from pygame.display import flip
 
-    # Initialize Pygame
+def main():
     pygame.init()
 
+    i = 100
+
     def create_main_surface():
-        # Tuple representing width and height in pixels
         screen_size = (1024, 768)
+        surface = pygame.display.set_mode(screen_size)
+        return surface
 
-        # Create window with given size
-        pygame.display.set_mode(screen_size)
+    surface = create_main_surface()
 
-                
-        while True:
-            pass
+    def render_frame(surface, x):
+        pygame.draw.circle(surface=surface, color="blue", center=(x,300), radius=50)
+    
 
-    create_main_surface()
+
+    while True:
+        render_frame(surface, i)
+        i += 1
+        flip()
+
+    
 main()
 
